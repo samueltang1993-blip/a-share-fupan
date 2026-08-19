69771360a563eaddd590f1a035e1dad55b69f101
samueltang1993-blip <samueltang1993@gmail.com>
2026-08-19 14:57:43 +0800

    data: 2026-08-18 复盘数据分块 08（ETF 行）

---
+++ data/data_08.js
@@ -0,0 +1,7 @@
+window.REVIEW_DATA=window.REVIEW_DATA||{};var R=window.REVIEW_DATA;
+R["etf"]=R["etf"]||{};R=R["etf"];
+R["rows"]=[{"code": "510300", "name": "沪深300ETF", "amt_today": 31.43, "shr_latest": 230.44, "shr_asof": "20260817", "shr_wow": -18.17, "shr_mom": -13.78}, {"code": "510500", "name": "中证500ETF", "amt_today": 25.06, "shr_latest": 51.42, "shr_asof": "20260817", "shr_wow": -2.24, "shr_mom": -10.31}, {"code": "512100", "name": "中证1000ETF", "amt_today": 34.2, "shr_latest": 66.81, "shr_asof": "20260817", "shr_wow": -13.48, "shr_mom": -37.66}, {"code": "588000", "name": "科创50ETF", "amt_today": 60.63, "shr_latest": 470.31, "shr_asof": "20260817", "shr_wow": -25.44, "shr_mom": 19.77}, {"code": "159915", "name": "创业板ETF", "amt_today": 43.3, "shr_latest": 171.14, "shr_asof": "20260817", "shr_wow": -14.6, "shr_mom": -5.25}];
+R=window.REVIEW_DATA;
+R["etf"]=R["etf"]||{};R=R["etf"];
+R["names"]={"510300": "沪深300ETF", "510500": "中证500ETF", "512100": "中证1000ETF", "588000": "科创50ETF", "159915": "创业板ETF"};
+R=window.REVIEW_DATA;
